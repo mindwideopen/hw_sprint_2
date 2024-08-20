9a4c4973436a62b77776905c9aa77e8ca7720f84
Nikolay Litvinov <nlit3926@gmail.com>
2024-08-20 21:12:49 +0200

    hw5 done

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { HashRouter } from 'react-router-dom'
 import { Layout } from './layout/Layout'
 import Pages from './Pages'
+import HW1 from "../hw01/HW1";
+import HW2 from "../hw02/HW2";
+import HW3 from "../hw03/HW3";
+import HW4 from "../hw04/HW4";
 
 /*
 * 1 - в файле Pages.tsx дописать роуты на все страницы
@@ -15,6 +19,7 @@ function HW5() {
             {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
             <Layout>
                 <Pages />
+
             </Layout>
         </HashRouter>
     )
--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -28,10 +28,13 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
 
                 <nav id={'hw5-menu'} className={s.nav}>
                     <NavLink
+
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : ""
+                        } // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +42,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : ""
+                        } // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +52,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : ""
+                        } // делает студент
                     >
                         Junior Plus
                     </NavLink>
